95f953b5c13510db05aaf9dd0b301152ba9b879f
Pioneer-Weirdo <liuhaijin99@gmail.com>
2023-01-14 14:49:58 +0800

    代码

--- MyBatis_mbg/src/test/java/com/zzuli/ExampleTest.java
+++ MyBatis_mbg/src/test/java/com/zzuli/ExampleTest.java
@@ -1,10 +1,14 @@
 package com.zzuli;
 
 import com.zzuli.entity.Emp;
+import com.zzuli.entity.EmpExample;
 import com.zzuli.mapper.EmpMapper;
 import com.zzuli.utils.SqlSessionUtil;
+import org.apache.ibatis.jdbc.Null;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ExampleTest {
     @Test
@@ -13,6 +17,39 @@ public class ExampleTest {
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
         Emp emp=mapper.selectByPrimaryKey(1);
         System.out.println(emp);
+        sqlSession.close();
+    }
+    @Test
+    public void test_selectByExample(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        EmpExample emp=new EmpExample();
+        /**
+         * QBC代码风格
+         */
+        emp.createCriteria().andGenderEqualTo("男").andAgeEqualTo(21);
+        List<Emp> emps=mapper.selectByExample(emp);
+        emps.forEach(System.out::println);
+        sqlSession.close();
+    }
+    @Test
+    public void test_update(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp=new Emp(1,"潘小雯",22,"女",null);
+        //int result=mapper.updateByPrimaryKey(emp);
+        //选择性修改，字段设置为空时不会修改字段默认值
+        int result=mapper.updateByPrimaryKeySelective(emp);
+        System.out.println(result);
+        sqlSession.close();
+    }
+    @Test
+    public void test_updateByExample(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp=new Emp(1,"潘小雯",22,"女",null);
+        int result=mapper.updateByPrimaryKey(emp);
+        System.out.println(result);
         sqlSession.close();
     }
 }
